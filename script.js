9a3e7d7bfd41fedf8f492e0fe9cd080be72d8bc6
susidharan2000 <susidharanraj2000@gmail.com>
2024-04-25 16:25:51 +0530

    updated

--- script.js
+++ script.js
@@ -2,12 +2,16 @@
 const card = document.querySelector('.card');
 const button = document.querySelector('.getImg-btn');
 
-button.addEventListener('click',async()=>{
-  const res = await fetch(`https://dog.ceo/api/breeds/image/random`);
-  const data = await res.json();
-  card.innerHTML = `
-  <img src="${data.message}"/>
-  `;
+button.addEventListener('click',()=>{
+  const res = fetch(`https://dog.ceo/api/breeds/image/random`);
+  res
+  .then((res)=>{ return res.json()})
+  .then((data)=>{
+    card.innerHTML = `
+    <img src="${data.message}"/>
+    `;
+  })
+  .catch((error)=>{console.log(error)});
 });
 
 
